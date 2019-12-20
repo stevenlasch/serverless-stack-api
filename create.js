6689aebce94dcb163d43a82a0a81395c4eb93587
Steven Lasch <steven.lasch@gmail.com>
2019-12-20 15:49:33 -0500

    Adds libs directory and refactors create.js

--- create.js
+++ create.js
@@ -1,16 +1,17 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// We are using the async/await pattern
+// This allows us to return once we are done processing; instead of using the callback function
 
-export function main(event, context, callback) {
+export async function main(event, context) {
     // Request body is passed in as a JSON encoded string in 'event.body'
     // This represents the HTTP request parameters.
     const data = JSON.parse(event.body);
 
     const params = {
-        // We read the DynamoDB table from the environment variable
-        TableName: process.env.tableName,
+        TableName: "notes",
         
         // 'Item' contains the attributes of the item to be created
         // - 'userId': user identities are federated through the Cognito Identity Pool, we will use the identity id as the user id of the authenticated user
@@ -28,32 +29,10 @@ export function main(event, context, callback) {
         }
     };
 
-    dynamoDb.put(params, (error, data) => {
-        // Set response headers to enable CORS (CrossOrigin Resource Sharing)
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        };
-
-        // Return status code 500 on error
-        if (error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({ status: false })
-            };
-
-            callback(null, response);
-            return;
-        }
-
-        // Return status code 200 and the newly created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify(params.Item)
-        };
-
-        callback(null, response);  
-    });
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
 }
